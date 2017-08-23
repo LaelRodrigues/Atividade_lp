b75dbb8a3779136ec5905ba69f902a3511a8608b
LaelRodrigues <laelrodrigues7@gmail.com>
2017-08-22 22:24:37 -0300

    Adicionando arquivo principal que simula a corrida de sapos

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include "sapo.h"
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl; 
+
+int Sapo::disTotalPercorrida = 0;
+
+int main() {
+	
+	int numSapos, disTotal;
+	cout << "--------------------Corrida de sapos-------------------" << endl;
+	cout << "Digite o numero de sapos que participaram da corrida: ";
+	cin >> numSapos;
+	cout << "Digite a distancia total da corrida: ";
+	cin >> disTotal;
+
+	Sapo::disTotalPercorrida = disTotal;
+
+	Sapo *sapos = new Sapo[numSapos];
+	for(int j = 0; j < numSapos; j++) {
+		sapos[j].setId(j+1);	
+	}
+
+	int i = 1;
+	int vencedor;
+	while(i) {
+		for(int j = 0; j < numSapos; j++) {
+			sapos[j].pular();
+		}
+		for(int j = 0; j < numSapos; j++) {
+			if(sapos[j].getDisPercorrida() > Sapo::disTotalPercorrida) {
+				vencedor = j;
+				i = 0;
+				break;
+			}
+		}
+	}
+	cout << endl;
+	cout << "O vencedor foi o sapo " << sapos[vencedor].getId() << endl; 
+	cout << "Numero de pulos dado = " << sapos[vencedor].getQuantPulos() << endl;
+	cout << "A distancia percorrida = " << sapos[vencedor].getDisPercorrida() << endl;
+}
